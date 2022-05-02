dbd7b901ba184481c1e9386086cb453d14fa36e8
lucasfs022 <lucasfs022@gmail.com>
2022-05-02 15:06:39 -0300

    alter height/css, correct accordion display

--- Javascript/script.js
+++ Javascript/script.js
@@ -2,6 +2,11 @@ const faqs = document.querySelectorAll(".faq")
 
 faqs.forEach(faq => {
     faq.addEventListener("click", () => {
-        faq.classList.toggle("active")
+        faqs.forEach(item => {
+            if (item !== faq) {
+                item.classList.remove("active");
+            }   
+        })
+        faq.classList.toggle("active");
     })
 })
